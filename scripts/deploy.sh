2ea9f9b731177c3c441ba8a0d391af0e8abae83a
gostonefire <peter.steneld@gmail.com>
2026-01-05 15:57:18 +0100

    adds deployer script

---
+++ scripts/deploy.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+REPO_NAME=$1
+DEV_DIR=$2
+SUB_SCRIPT_LOG=$3
+
+HOME="/home/petste"
+APP_DIR="MyWeatherLogger"
+SERVICE_NAME="weatherlogger.service"
+
+if [ -z "$REPO_NAME" ] || [ -z "$DEV_DIR" ] || [ -z "$SUB_SCRIPT_LOG" ]; then
+  echo "Usage: $0 <repo_name> <dev_dir> <sub_script_log>"
+  exit 1
+fi
+
+# Get the owner of the DEV_DIR
+DEV_USER=$(stat -c '%U' "$DEV_DIR")
+
+# Function containing the logic to be run as the directory owner
+run_as_user() {
+  mkdir -p "$HOME/$APP_DIR/config" >> "$SUB_SCRIPT_LOG" 2>&1
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "could not create $HOME/$APP_DIR/config..."
+    exit $EXIT_CODE
+  fi
+
+  mkdir -p "$HOME/$APP_DIR/logs" >> "$SUB_SCRIPT_LOG" 2>&1
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "could not create $HOME/$APP_DIR/logs..."
+    exit $EXIT_CODE
+  fi
+
+  mkdir -p "$HOME/$APP_DIR/last_version" >> "$SUB_SCRIPT_LOG" 2>&1
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "could not create $HOME/$APP_DIR/last_version..."
+    exit $EXIT_CODE
+  fi
+
+  # shellcheck disable=SC2164
+  cd "$DEV_DIR"/"$REPO_NAME" >> "$SUB_SCRIPT_LOG" 2>&1
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "could not change directory to $DEV_DIR"/"$REPO_NAME..."
+    exit $EXIT_CODE
+  fi
+
+  if [ -f "$HOME/$APP_DIR/$REPO_NAME" ]; then
+    mv "$HOME/$APP_DIR/$REPO_NAME" "$HOME/$APP_DIR/last_version/"
+  fi
+
+  cp "./target/release/$REPO_NAME" "$HOME/$APP_DIR/" >> "$SUB_SCRIPT_LOG" 2>&1
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "could not copy ./target/release/$REPO_NAME to $HOME/$APP_DIR/..."
+    exit $EXIT_CODE
+  fi
+
+  ### Add any extra deploy features to be run as dev user from here ###
+  mkdir -p "$HOME/$APP_DIR/db"
+
+  cp "./systemd/$SERVICE_NAME" "$HOME/$APP_DIR/" >> "$SUB_SCRIPT_LOG" 2>&1
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "could not copy ./systemd/$SERVICE_NAME to $HOME/$APP_DIR/..."
+    exit $EXIT_CODE
+  fi
+
+  cp "./systemd/start.sh" "$HOME/$APP_DIR/" >> "$SUB_SCRIPT_LOG" 2>&1
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "could not copy ./systemd/start.sh to $HOME/$APP_DIR/..."
+    exit $EXIT_CODE
+  fi
+
+  chmod 755 "$HOME/$APP_DIR/start.sh"
+
+  ### until to here! ##################################################
+}
+
+########## From here the script will be run as root, so add any commands such as systemctl etc. from here ##########
+if systemctl is-active --quiet "$SERVICE_NAME"; then
+  systemctl stop --quiet "$SERVICE_NAME"
+fi
+########## until to here! ##########################################################################################
+
+# Export variables so the subshell can see them, then run the function as the owner
+export REPO_NAME DEV_DIR HOME APP_DIR SUB_SCRIPT_LOG SERVICE_NAME
+
+sudo -u "$DEV_USER" -E bash -c "$(declare -f run_as_user); run_as_user"
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  exit $EXIT_CODE
+fi
+
+########## From here the script will be run as root, so add any commands such as systemctl etc. from here ##########
+cp "$HOME/$APP_DIR/$SERVICE_NAME" /lib/systemd/system/ >> "$SUB_SCRIPT_LOG" 2>&1
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "failed to copy $SERVICE_NAME to /lib/systemd/system/..."
+  exit $EXIT_CODE
+fi
+
+systemctl daemon-reload
+
+if ! systemctl is-enabled --quiet "$SERVICE_NAME"; then
+  systemctl enable "$SERVICE_NAME" >> "$SUB_SCRIPT_LOG" 2>&1
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "could not enable $SERVICE_NAME..."
+    exit $EXIT_CODE
+  fi
+fi
+
+systemctl start "$SERVICE_NAME" >> "$SUB_SCRIPT_LOG" 2>&1
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "could not start $SERVICE_NAME..."
+  exit $EXIT_CODE
+fi
+
+########## until to here! ##########################################################################################
